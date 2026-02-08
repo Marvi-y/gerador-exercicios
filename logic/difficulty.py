480a1259998de84806db34fc5324e7fbbf4c8111
Marvi-y <marvioz007@gmail.com>
2026-02-08 13:48:09 -0300

    Add files via upload

---
+++ logic/difficulty.py
@@ -0,0 +1,7 @@
+def dificuldade_por_nivel(nivel, lang):
+    if nivel == 1:
+        return "Fácil" if lang == "pt" else "Easy"
+    elif nivel == 2:
+        return "Médio" if lang == "pt" else "Medium"
+    else:
+        return "Difícil" if lang == "pt" else "Hard"
