34f4e56f02acb42272f7da8a8e140fe16d305c30
Marvi-y <marvioz007@gmail.com>
2026-02-08 12:48:48 -0300

    Add files via upload

---
+++ logic/stats.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+def calcular_estatisticas(historico):
+    if not historico:
+        return None
+
+    total = len(historico)
+    acertos = sum(1 for h in historico if h["correto"])
+    erros = total - acertos
+
+    taxa_acerto = round((acertos / total) * 100, 1)
+
+    operacoes = [
+        h["expressao"].split()[1]
+        for h in historico
+        if not h["correto"]
+    ]
+
+    erros_por_operacao = Counter(operacoes)
+
+    return {
+        "total": total,
+        "acertos": acertos,
+        "erros": erros,
+        "taxa_acerto": taxa_acerto,
+        "erros_por_operacao": erros_por_operacao
+    }
