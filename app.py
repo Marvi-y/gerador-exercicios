8e757c3d4de01dc40202ac4144504986ab2b3b17
Marvi-y <marvioz007@gmail.com>
2026-02-07 21:45:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if st.session_state.erros:
     
 
 if st.session_state.modo_treino_erros:
-if st.button("⬅️ Voltar ao modo normal"):
+if st.button(T["back_normal"]):
 st.session_state.modo_treino_erros = False
 gerar_exercicio()
 
