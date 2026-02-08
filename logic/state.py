767ba1a70a282fd9b1c206656e5a73058e9c200b
Marvi-y <marvioz007@gmail.com>
2026-02-08 16:36:38 -0300

    Update state.py

--- logic/state.py
+++ logic/state.py
@@ -1,5 +1,14 @@
 import streamlit as st
+def init_state():
+    if "operacoes_ativas" not in st.session_state:
+        st.session_state.operacoes_ativas = ["+", "-", "*"]
 
+    if "a" not in st.session_state:
+        st.session_state.a = None
+    if "b" not in st.session_state:
+        st.session_state.b = None
+    if "operacao" not in st.session_state:
+        st.session_state.operacao = None
 def init_state():
     defaults = {
         "nivel": 1,
