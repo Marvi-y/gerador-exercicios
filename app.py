1f94f6b52d7aea9f40176c2a004c16b482aa79f3
Marvi-y <marvioz007@gmail.com>
2026-02-07 21:26:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ st.write(f"🔥 {T['streak']}: {st.session_state.acertos}/3")
 if st.session_state.erros:
     st.button(T["train_errors"])
     st.session_state.modo_treino_erros = True
-        gerar_exercicio()
+     gerar_exercicio()
 
 if st.session_state.modo_treino_erros:
     if st.button("⬅️ Voltar ao modo normal"):
